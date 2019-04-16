e7ebc24e4754d994ffd908b4e3dd0b60cdcafd91
GLissovskiy <glissovskiy@gmail.com>
2019-04-17 00:48:47 +0600

    2.0. Видео[#132120]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,12 @@
 package ru.job4j.calculate;
 
 /**
-* Calculate
-*
-* @author glissovskiy (leginBSD@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Calculate
+ *
+ * @author glissovskiy (leginBSD@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Calculate {
     /**
