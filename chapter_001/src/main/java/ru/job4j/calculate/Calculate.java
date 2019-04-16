b51547d50d0112acd1f5feecb932d05799164f11
GLissovskiy <glissovskiy@gmail.com>
2019-04-16 23:18:15 +0600

    1.3. JavaDoc. Code convention[#132165]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,11 +1,27 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author glissovskiy (leginBSD@gmail.com)
+*/
+
 public class Calculate{
+	
+	/**
+	* Method echo
+	* @param value Your for 3x value.
+	* @return Echo value 3x.
+	*/
 	
 	public String echo(String value){
 		return String.format("%s, %s, %s", value, value, value);
 	}
 	
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main (String[] args){
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("aah"));
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author glissovskiy Arsentev (legionBSD@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
