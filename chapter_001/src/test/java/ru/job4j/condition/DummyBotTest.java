974da432d5dac65771bc1461b476a84841eaf703
GLissovskiy <glissovskiy@gmail.com>
2019-04-21 18:18:20 +0600

    4.1. Глупый бот.[#132122]

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author glissovskiy (legionBSD@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(dummyBot.answer("Привет, Бот."),
+                is("Привет, умник."));
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(
+                dummyBot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(
+                dummyBot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
