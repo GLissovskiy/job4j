c7824b52b905f995d696e4611c90c8e534912ca5
GLissovskiy <glissovskiy@gmail.com>
2019-06-12 17:27:09 +0600

    4. Используя шаблон проектирования - стратегию[#132208]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -0,0 +1,21 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author legionbsd (legionbsd@gmail.com).
+ * @version $Id$
+ * @since 0.1
+ */
+public class Triangle implements Shape {
+    @Override
+    public String draw() {
+        StringBuilder pic = new StringBuilder();
+        pic.append("   +   ");
+        pic.append(System.lineSeparator());
+        pic.append("  + +  ");
+        pic.append(System.lineSeparator());
+        pic.append(" + + + ");
+        pic.append(System.lineSeparator());
+        pic.append("+ + + +");
+        return pic.toString();
+    }
+}
